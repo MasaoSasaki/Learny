69cfa9121e096f5ac4435aa3b512495d80a8c581
MasaoSasaki <daikichi852@icloud.com>
2021-03-18 09:22:54 +0900

    :sparkles: Add learning page

---
+++ src/pages/learning.tsx
@@ -0,0 +1,8 @@
+import type { VFC } from "react";
+import { Layout } from "src/components/layout";
+
+const Learning: VFC = () => {
+  return <Layout>Learning</Layout>;
+};
+
+export default Learning;

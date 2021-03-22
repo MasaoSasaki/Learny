0e27d6fd1012c34f034478cd37f029baa9bf2f98
MasaoSasaki <daikichi852@icloud.com>
2021-03-22 15:47:16 +0900

    :sparkles: Add json place holder request

--- src/components/learning-config/index.tsx
+++ src/components/learning-config/index.tsx
@@ -1,9 +1,11 @@
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
-export function LearningConfig(): JSX.Element {
+type Props = { workCount: number };
+
+export function LearningConfig({ workCount }: Props): JSX.Element {
   return (
-    <div className="w-full lg:w-5/12 rounded-lg lg:rounded-l-lg shadow-2xl bg-white opacity-75 mx-4 my-12 dark:bg-gray-900" >
-      <div className="p-4 md:p-12 text-center lg:text-left dark:bg-gray-800"></div>
+    <div className="w-full lg:w-5/12 rounded-lg lg:rounded-l-lg shadow-2xl bg-white opacity-75 mx-4 my-12 dark:bg-gray-900">
+      <div className="p-4 md:p-12 text-center lg:text-left dark:bg-gray-800">{workCount}</div>
     </div>
   );
 }
--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,17 +1,23 @@
-import type { VFC } from "react";
 import { Layout } from "src/components/layout";
 import { ProfileCard } from "../components/profile-card";
 import { LearningConfig } from "../components/learning-config";
+import { GetServerSideProps } from "next";
 
-const MyPage: VFC = () => {
+type Props = { workCount: number };
+
+export default function MyPage(props: Props): JSX.Element {
   return (
     <Layout>
       <div className="max-w-4xl lg:max-w-6xl flex justify-around items-center h-auto flex-wrap mx-auto my-20 lg:my-0">
         <ProfileCard />
-        <LearningConfig />
+        <LearningConfig workCount={props.workCount} />
       </div>
     </Layout>
   );
 };
 
-export default MyPage;
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  return { props: { workCount: data.length } }
+}

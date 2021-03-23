175b413dec367ebbec0c5b00de3e35f237e0d2f6
MasaoSasaki <daikichi852@icloud.com>
2021-03-23 15:07:32 +0900

    :zap: Add query parameter

--- src/components/learning-config/index.tsx
+++ src/components/learning-config/index.tsx
@@ -1,11 +1,53 @@
 import styles from "./index.module.scss";
+import { NextRouter, useRouter } from "next/router";
+import { useState } from "react";
 
 type Props = { workCount: number };
 
 export function LearningConfig({ workCount }: Props): JSX.Element {
+  const router: NextRouter = useRouter();
+  const [random, setRandom] = useState<boolean>(true);
+  const [unAnswered, setUnAnswered] = useState<boolean>(true);
+  const clickButton = () => {
+    router.push({
+      pathname: "/learning",
+      query: { random: random, unAnswered: unAnswered },
+    });
+  };
+  console.log(random, unAnswered);
   return (
     <div className="w-full lg:w-5/12 rounded-lg lg:rounded-l-lg shadow-2xl bg-white opacity-75 mx-4 my-12 dark:bg-gray-900">
-      <div className="p-4 md:p-12 text-center lg:text-left dark:bg-gray-800">{workCount}</div>
+      <div className="p-4 md:p-12 text-center lg:text-left dark:bg-gray-800">
+        {workCount}
+        <div className="block text-left">
+          <span className="text-gray-700">出題設定</span>
+          <div className="mt-2">
+            <div>
+              <input type="checkbox" className="form-checkbox" defaultChecked id="random" />
+              <label className="inline-flex items-center" htmlFor="random" onClick={() => setRandom(!random)}>
+                <span className="ml-2">出題順をランダムにする。</span>
+              </label>
+            </div>
+            <div>
+              <input type="checkbox" className="form-checkbox" defaultChecked id="unAnswered" />
+              <label
+                className="inline-flex items-center"
+                htmlFor="unAnswered"
+                onClick={() => setUnAnswered(!unAnswered)}
+              >
+                <span className="ml-2">未回答を優先する。(全体の50%)</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+          onClick={() => clickButton()}
+        >
+          開始する
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/learning.tsx
+++ src/pages/learning.tsx
@@ -1,8 +1,22 @@
-import type { VFC } from "react";
 import { Layout } from "src/components/layout";
+import { GetServerSideProps } from "next";
 
-const Learning: VFC = () => {
+type Props = {
+  workCount: number;
+  data: any;
+};
+
+export default function Learning(props: Props): JSX.Element {
   return <Layout>Learning</Layout>;
 };
 
-export default Learning;
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const res = await fetch("http://localhost:3000/users/2/works");
+  const data = await res.json();
+  return {
+    props: {
+      data: data,
+      workCount: data.length,
+    },
+  };
+};

bf02f02db20d8c251facc09e24164b74c681ed2a
MasaoSasaki <daikichi852@icloud.com>
2021-05-18 21:26:35 +0900

    :card_file_box: Add function "timeStamp"

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -6,3 +6,18 @@ export const shuffle = <T extends {}>([...array]: T[]): T[] => {
   }
   return array;
 };
+
+// timestampの時間が数字一桁なら先頭に"0"を追加する
+export const timeStamp = (): string => {
+  const addDateZero = (value: number): string => {
+    return value <= 9 ? `0${value}` : `${value}`;
+  };
+  const currentTime = new Date();
+  const Year:string = String(currentTime.getFullYear());
+  const Month:string = addDateZero(currentTime.getMonth() + 1);
+  const Day:string = addDateZero(currentTime.getDate());
+  const Hour:string = addDateZero(currentTime.getHours());
+  const Min:string = addDateZero(currentTime.getMinutes());
+  const Sec:string = addDateZero(currentTime.getSeconds());
+  return `${Year}-${Month}-${Day} ${Hour}:${Min}:${Sec}`;
+};
